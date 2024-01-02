5fd53c9484aeba171ec8ad41babd62cc8df776cb
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-02 11:31:35 +0800

    first commit

--- resources/views/simple.blade.php
+++ resources/views/simple.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<p>I wish I were a <?php echo $squirrel; ?> squirrel!</p>
+    <p>I wish I were a <b>{{ $squirrel }}</b> squirrel!</p>
 
 </body>
+
 </html>
