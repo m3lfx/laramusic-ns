88a23e56e1244e885b129208f056a458243e4720
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-16 11:08:59 +0800

    album store

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -20,4 +20,20 @@ class AlbumController extends Controller
         $artists = Artist::all();
         return View::make('album.create', compact('artists'));
     }
+
+    public function store(Request $request)
+    {
+        // dd($request);
+        $album = new Album();
+        $album->title = $request->title;
+        $album->genre = $request->genre;
+        $album->date_released = $request->date_released;
+        $album->artist_id = $request->artist_id;
+        
+        $album->save();
+        return Redirect::to('album');
+        
+        
+        
+    }
 }
