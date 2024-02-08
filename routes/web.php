72418bedc949e078f670c2cbf574b595d36e6faf
rommel dalisay <rommelmdalisay@gmail.com>
2024-02-08 14:39:01 +0800

    authentication routes

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,28 @@ Route::get('/artist/{id}/edit', [ArtistController::class, 'edit']);
 Route::post('/artist/{id}/update', [ArtistController::class, 'update']);
 Route::get('/artist/{id}/delete', [ArtistController::class, 'delete']);
 
-Route::prefix('album')->group(function () {
-    Route::get('/', [AlbumController::class, 'index'])->name('album');
-    Route::get('/create', [AlbumController::class, 'create'])->name('album.create');
-    Route::post('/store', [AlbumController::class, 'store'])->name('album.store');
-    Route::get('/{id}/edit', [AlbumController::class, 'edit'])->name('album.edit');
-    Route::post('/{id}/update', [ArtistController::class, 'update'])->name('album.update');
-    Route::get('/{id}/delete', [ArtistController::class, 'delete'])->name('album.delete');
-});
+
 Route::get('/listeners/{id}/restore',  [ListenerController::class, 'restore'])->name('listeners.restore');
 // Route::get('/listeners/add-albums', [ListenerController::class, 'addAlbums'])->name('listeners.addAlbums');
 Route::post('/songs-search',  [SongController::class, 'search'])->name('songs.search');
 
-Route::get('/listeners/add-album', [ListenerController::class, 'addAlbums'])->name('listeners.addAlbums');
+Route::get('/listeners/add-album', [ListenerController::class, 'addAlbums'])->name('listeners.addAlbums')->middleware('auth');
 Route::post('/listeners/add-album', [ListenerController::class, 'addAlbumListener'])->name('listeners.addAlbumListener');
-Route::resource('songs', SongController::class);
-Route::resource('listeners', ListenerController::class);
+// Route::resource('songs', SongController::class)->middleware('auth');
+// Route::resource('listeners', ListenerController::class)->middleware('auth');
 
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('album')->group(function () {
+        Route::get('/', [AlbumController::class, 'index'])->name('album');
+        Route::get('/create', [AlbumController::class, 'create'])->name('album.create');
+        Route::post('/store', [AlbumController::class, 'store'])->name('album.store');
+        Route::get('/{id}/edit', [AlbumController::class, 'edit'])->name('album.edit');
+        Route::post('/{id}/update', [ArtistController::class, 'update'])->name('album.update');
+        Route::get('/{id}/delete', [ArtistController::class, 'delete'])->name('album.delete');
+    });
+    Route::resource('songs', SongController::class);
+    Route::resource('listeners', ListenerController::class);
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
